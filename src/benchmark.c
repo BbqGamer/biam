ee8ba5dc763aadf32bfe041f50a0a7216fab92f6
BbqGamer <korba.adam@gmail.com>
2025-03-06 22:59:36 +0100

    Improve benchmarking function

--- src/benchmark.c
+++ src/benchmark.c
@@ -14,9 +14,15 @@ int main() {
     int counter = 0;
     time_t start = time(NULL);
 
+    printf("Running benchmark for at least %d seconds and %d iterations\n",
+        MINIMUM_TIME, MINIMUM_ITERS);
+
     do {
         counter += 1;
         printf("%d\n", counter);
         alg();
     } while (counter <= MINIMUM_ITERS || time(NULL) - start <= MINIMUM_TIME);
+
+    printf("Average runtime of alg() is: %f seconds",
+         (time(NULL) - start)/(float)counter);
 }
